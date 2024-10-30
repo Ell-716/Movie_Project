dc1a0a01f41f60b0917bbfc2a5645fea137c7e0c
Ell-716 <elenabai.2021@gmail.com>
2024-10-30 17:00:10 +0100

    Generates website

--- movie_app.py
+++ movie_app.py
@@ -420,7 +420,35 @@ class MovieApp:
             print(Fore.RED + "No movies match the filter criteria.")
 
     def _generate_website(self):
-        ...
+        movies = self._storage.list_movies()
+
+        movie_grid = ""
+        for title, details in movies.items():
+            movie_grid += f"""
+                <li>
+                    <div class="movie">
+                        <img class="movie-poster" src="{details['Poster']}"/>
+                        <div class="movie-rating">
+                            <svg width="10" height="10" fill="gold" viewBox="0 0 24 24">
+                                <path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path>
+                            </svg>
+                            <span class="rating">{details['Rating']}</span>
+                        </div>
+                        <div class="movie-title">{title}</div>
+                        <div class="movie-year">{details['Year']}</div>                     
+                    </div>
+                </li>
+                """
+        with open("_static/index_template.html", "r") as template_file:
+            template_content = template_file.read()
+
+        final_html = template_content.replace("__TEMPLATE_TITLE__", "My Movie App")
+        final_html = final_html.replace("__TEMPLATE_MOVIE_GRID__", movie_grid)
+
+        with open("_static/index.html", "w") as output_file:
+            output_file.write(final_html)
+
+        print(Fore.GREEN + "Website was generated successfully.")
 
     def run(self):
         """
